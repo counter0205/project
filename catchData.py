9aeb2e32489882519c36ef8f8392c52ab8325c09
counter0205 <counter0205@users.noreply.github.com>
2015-09-02 14:50:55 +0800

    Update catchData.py

--- catchData.py
+++ catchData.py
@@ -20,7 +20,7 @@ list = io.open(u'C:/judicial/list.txt','a+',encoding='utf-8')
 
 for reading in range (0,len(open_list)):
     list.write((open_list[reading]+'\n'))
-    print 'checkpoint1'
+    #print 'checkpoint1'
 #list.seek(0,0)#游標移回開頭讀取
 cont=list.read()
 list.close()
@@ -31,7 +31,7 @@ for year in range(89,105):
     month=1
     day=1
     interval_month=3 #每次取幾個月，請給可整除12個月的數字，不然會少抓資料
-    print 'checkpoint2'
+    #print 'checkpoint2'
     for mon in range(1,((12/interval_month)+1)):
         driver.get('http://jirs.judicial.gov.tw/FJUD/FJUDQRY01_1.aspx')
         driver.implicitly_wait(30)
@@ -51,24 +51,25 @@ for year in range(89,105):
         driver.find_element_by_name("dm2").send_keys(str(month+interval_month-1))
         driver.find_element_by_name("dd2").clear()
         driver.find_element_by_name("dd2").send_keys(str(days[month+interval_month-2]))
+        print '現在抓取'+str(year)+'年'+str(month)+'月到'+str(month+interval_month-1)+'月，GoodLuck'
         time.sleep(5)
         driver.find_element_by_name("Button").click()
         month=month+interval_month
-
         time.sleep(5)
         raw_page=driver.page_source
         #範圍錯誤處理
         over_num=re.findall(u'超出 200 筆',raw_page)
-        print 'checkpoint3'
+        #print 'checkpoint3'
         if(len(over_num)!=0):
             print (u'請修正範圍')
             driver.close()
             print('check2')
             sys.exit()
-
-        print 'checkpoint4'
+        page=re.search(u'共\s+(\d+)\s+筆 / 每頁\s+(\d+)\s+筆 / 共\s+(\d+)\s+頁 / 現在第\s+(\d+)\s+頁',raw_page)
+        total_page=int(page.group(3))
+        #print 'checkpoint4'
         for pages in range(0,total_page):
-            print 'checkpoint5'
+            #print 'checkpoint5'
             time.sleep(5)
             raw_page=driver.page_source
             page=re.search(u'共\s+(\d+)\s+筆 / 每頁\s+(\d+)\s+筆 / 共\s+(\d+)\s+頁 / 現在第\s+(\d+)\s+頁',raw_page)
@@ -95,16 +96,16 @@ for year in range(89,105):
             'Accept-Encoding':'gzip, deflate',
             'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
             }
-            print 'checkpoint6'
+            #print 'checkpoint6'
             for row in range(0,index):
-                print 'checkpoint7'
+                #print 'checkpoint7'
                 list = open('C:/judicial/list.txt','r')
                 cont=list.read()
                 list.close()
                 finding=re.findall(((get_word[row])+".txt").encode('utf-8'),cont)
                 #print len(finding)
                 if(len(finding)==0):
-                    print 'checkpoint8'
+                    #print 'checkpoint8'
                     name=get_word[row]
                     print name
                     replace_url=re.sub("amp;", "", raw_url[row])
@@ -114,7 +115,8 @@ for year in range(89,105):
                     url=final_url
                     res=requests.get(url,headers=head4)
                     error=re.findall(u'裁判字號',res.text)
-                    
+                    ruling=re.findall(u'<pre>[^0-9]{6,10}法院民事裁定　',res.text)
+                    judgement=re.findall(u'<pre>[^0-9]{6,10}法院民事判決　',res.text)
                     if len(error)==0:
                         driver.close()
                         print ('check3')
@@ -126,21 +128,28 @@ for year in range(89,105):
                     #soup = BeautifulSoup(res.text.encode('utf-8'), "html.parser")
        
                     #print soup
-                    f = open('C:/judicial/%s.txt' %(soup.findAll('td')[6].text), 'w')
-                    for i in range(5,11):
-                        f.write(soup.findAll('td')[i].text.encode('utf-8'))
-                        if i % 2 == 0:
-                            f.write('\n')
-                    f.write(soup.findAll('td')[13].text.encode('utf-8'))
-                    f.close()
-                    list = open('C:/judicial/list.txt','a+')
-                    list.write((name+'.txt').encode('utf-8')+'\n')
-                    list.close()
-                    list2 = open('C:/judicial/list2.txt','a+')
-                    list2.write((name+'.txt').encode('utf-8')+'\n')
-                    list2.close()
+               
+                    if len(judgement)==1:
+                        f = open('C:/judicial/%s.txt' %(soup.findAll('td')[6].text), 'w')
+                        for i in range(5,11):
+                            f.write(soup.findAll('td')[i].text.encode('utf-8'))
+                            if i % 2 == 0:
+                                f.write('\n')
+                        f.write(soup.findAll('td')[13].text.encode('utf-8'))
+                        f.close()
+                        list = open('C:/judicial/list.txt','a+')
+                        list.write((name+'.txt').encode('utf-8')+'\n')
+                        list.close()
+                        list2 = open('C:/judicial/list2.txt','a+')
+                        list2.write((name+'.txt').encode('utf-8')+'\n')
+                        list2.close()
+                    elif len(ruling)==1:
+                        list_ruling = open('C:/judicial/list_ruling.txt','a+')
+                        list_ruling.write((name+'.txt').encode('utf-8')+'\n')
+                    else:
+                        print 'checkprocess'
             if total_page>1 and total_page!=now_page:
-                print 'checkpoint9'
+                #print 'checkpoint9'
                 driver.find_element_by_link_text(u"下一頁").click()
                 time.sleep(8)
 
